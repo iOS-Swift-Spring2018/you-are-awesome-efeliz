e89cc1e0fefb1e56b6418efcd47a26e9eed06959
Estevan Feliz <felize@bc.edu>
2018-03-09 12:50:07 -0500

    Update: Final Version

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,17 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
+    @IBOutlet weak var awesomeImage: UIImageView!
+    @IBOutlet weak var soundSwitch: UISwitch!
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    let numberOfImages = 8
+    var soundNumber = -1
+    let numberOfSounds = 6
     
     // Code below executes when the app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    // MARK: - My Own Functions
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        // Can we load the sound?
+        if let sound = NSDataAsset(name:soundName){
+            // check if sound.data is a sound file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            // if reading in the NSDataAsset didn't work, tell the user / report the error.
+            print("ERROR: file \(soundName) didn't load.")
+        }
+    }
     
+    func randomNumber(lastNum: Int, maximum: Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maximum)))
+        } while lastNum == newIndex
+        return newIndex
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if !soundSwitch.isOn{
+            // Stop playing
+            if awesomePlayer.isPlaying {
+                awesomePlayer.stop()
+            }
+        }
+    }
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         
         let messages = ["You Are Fantastic!",
@@ -26,11 +69,30 @@ class ViewController: UIViewController {
                         "When the Genius Bar needs help, they call you!",
                         "You Brighten My Day!"]
         
+        index = randomNumber(lastNum: index, maximum: messages.count)
+        messageLabel.text = messages[index]
+        
+        awesomeImage.isHidden = false
+        imageNumber = randomNumber(lastNum: imageNumber, maximum: numberOfImages)
+        awesomeImage.image = UIImage(named: "image\(imageNumber)")
+        
+        // Play a sound
+        if soundSwitch.isOn == true {
+            soundNumber = randomNumber(lastNum: soundNumber, maximum: numberOfSounds)
+            let soundName = "sound" + String(soundNumber)
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
+        }
+        
+        //        var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        //        messageLabel.text = messages[randomIndex]
+        
+        /*
         index += 1
         if index == messages.count {
             index = 0
         }
         messageLabel.text = messages[index]
+        */
         
 //        let message1 = "You Are Da Bomb!"
 //        let message2 = "You Are Great!"
